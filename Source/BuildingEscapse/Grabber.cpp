a94e72b6ff0784c8b12946ab11acefedecbd8bdc
Josh <joshdgibson21@outlook.com>
2019-04-22 15:53:07 +0100

    The grabber component now draws a debug line in front of the actor

--- Source/BuildingEscapse/Grabber.cpp
+++ Source/BuildingEscapse/Grabber.cpp
@@ -1,9 +1,13 @@
 // (learning unreal)
 
 #include "Grabber.h"
+#include "Gameframework/Actor.h"
+
+#include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
+	: m_playerControllerPtr(nullptr)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -20,14 +24,30 @@ void UGrabber::BeginPlay()
 
 	UE_LOG(LogTemp, Warning, TEXT("UGrabber::BeginPlay() called"));
 
+	m_playerControllerPtr = GetWorld()->GetFirstPlayerController();
+	if (!m_playerControllerPtr)
+	{ 
+		UE_LOG(LogTemp, Error, TEXT("UGrabber couldn't get a pointer to the default player controller"));
+	}
 }
 
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
+	// remember that float values are in cm
+
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	m_playerControllerPtr->GetPlayerViewPoint(m_position, m_rotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("UGrabber::BeginPlay() called"));
+	const FString posStr = m_position.ToString();
+	const FString rotationStr = m_rotation.ToString();
+	const FVector rotationAsUnitVector = m_rotation.Vector();
+	UE_LOG(LogTemp, Warning, TEXT("UGrabber is positioned at %s and has rotation %s facing direction %s"), *posStr, *rotationStr, *rotationAsUnitVector.ToString());
+	constexpr FColor debugLineColour(255, 0, 0);
+	const FVector debugLineEndPos = m_position + (rotationAsUnitVector * Reach);
+	DrawDebugLine(GetWorld(), m_position, debugLineEndPos, debugLineColour, false, 0.0f, 0, 10.0f);
 }
 
--- Source/BuildingEscapse/Grabber.h
+++ Source/BuildingEscapse/Grabber.h
@@ -24,5 +24,13 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	APlayerController* m_playerControllerPtr;
+	FVector m_position;
+	FRotator m_rotation;
+
+	// how long the grabber's reach is, this is in cm
+	UPROPERTY(EditAnywhere)
+	float Reach = 100.0f; // default value
+
 };

1dac01dfaecf27a953d85c3f30e96c131b2f9b6c
Josh <joshdgibson21@outlook.com>
2019-04-22 19:16:18 +0100

    The Grabber class now performs ray cast checks of physics actors, the Chair and rock now simulate physics and are classed as physics actors

--- Source/BuildingEscapse/Grabber.cpp
+++ Source/BuildingEscapse/Grabber.cpp
@@ -3,7 +3,10 @@
 #include "Grabber.h"
 #include "Gameframework/Actor.h"
 
+#include <Runtime/Engine/Classes/Engine/World.h>
+
 #include "DrawDebugHelpers.h"
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -41,13 +44,38 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 
 	m_playerControllerPtr->GetPlayerViewPoint(m_position, m_rotation);
 
+	UWorld* worldPtr = GetWorld();
+
 	UE_LOG(LogTemp, Warning, TEXT("UGrabber::BeginPlay() called"));
 	const FString posStr = m_position.ToString();
 	const FString rotationStr = m_rotation.ToString();
 	const FVector rotationAsUnitVector = m_rotation.Vector();
 	UE_LOG(LogTemp, Warning, TEXT("UGrabber is positioned at %s and has rotation %s facing direction %s"), *posStr, *rotationStr, *rotationAsUnitVector.ToString());
 	constexpr FColor debugLineColour(255, 0, 0);
-	const FVector debugLineEndPos = m_position + (rotationAsUnitVector * Reach);
-	DrawDebugLine(GetWorld(), m_position, debugLineEndPos, debugLineColour, false, 0.0f, 0, 10.0f);
+	const FVector lineEndPos = m_position + (rotationAsUnitVector * Reach);
+	DrawDebugLine(worldPtr, m_position, lineEndPos, debugLineColour, false, 0.0f, 0, 10.0f);
+
+	
+	// ray cast checks
+	FHitResult hitResult;
+	FCollisionQueryParams queryParams(FName(TEXT("")), false, GetOwner());
+
+	bool hit =
+	worldPtr->LineTraceSingleByObjectType(
+		hitResult,
+		m_position,
+		lineEndPos,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		queryParams
+	);
+
+	if (hit)
+	{
+		const AActor* hitActor = hitResult.GetActor();
+		if (hitActor)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("The ray cast has hit %s"), *hitActor->GetName());
+		}
+	}
 }
 
